fec1d4cd2caee2bdfd30954cb29e9002dd131a0d
Antonio <aschiavon91@gmail.com>
2023-03-06 14:08:37 -0300

    feat: resolve all todos

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for typos.
 GH_REPO="https://github.com/crate-ci/typos"
 TOOL_NAME="typos"
 TOOL_TEST="typos --version"
@@ -27,12 +26,10 @@ sort_versions() {
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    sed 's/^v//'
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if typos has other means of determining installable versions.
   list_github_tags
 }
 
@@ -46,7 +43,6 @@ download_release() {
     rm -fr "${release_file}"
   fi
 
-  # TODO: Adapt the release URL convention for typos
   url="$GH_REPO/releases/download/v${version}/${file_name}"
 
   echo "* Downloading $TOOL_NAME release $version..."
@@ -66,7 +62,6 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Assert typos executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
